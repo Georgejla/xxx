ab299bd992dc5bd57c086e0655c80f7f35367858
xxx <georgejla@hotmail.com>
2021-08-30 07:49:10 -0300

    4to intento

---
+++ prueba.js
@@ -0,0 +1,20 @@
+function mayorQueCincuenta(num) {
+  // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
+  // De lo contrario, devuelve "false"
+  // Tu código:
+  if (num > 50) {
+    return console.log(true);
+  }
+  return console.log(false);
+}
+
+function deEuroAdolar(euro){
+  //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
+  //como parámetro un número de euros y calcule el cambio en dólares.
+  //Escribe tu código aquí
+  var deEuroAdolar = euro * 1.20;
+  return console.log(deEuroAdolar);
+}
+deEuroAdolar(234)
+mayorQueCincuenta(25
+)
